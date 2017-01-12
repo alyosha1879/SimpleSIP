97df12c72b2f3e079b6f17c7468bcf7f89230c48
Okano Yuu <alexei.fyodorovich.karamazov@gmail.com>
2017-01-12 23:42:33 +0900

    add note-code.

---
+++ note.py
@@ -0,0 +1,26 @@
+def handle_Request():
+    pass
+
+def handle_Response():
+    pass
+
+def handle_Request_Invite():
+    pass
+
+def handle_Request_Register():
+    pass
+
+def handle_Request_ACK():
+    pass
+
+def handle_Request_CANCEL():
+    pass
+
+def handle_Response_200OK():
+    pass
+
+def handle_Response_180Trying():
+    pass
+
+
+
