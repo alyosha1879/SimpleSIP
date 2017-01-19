42b7081bcb9a9cd259ae3ce3dd01167b10d4bba1
Okano Yuu <alexei.fyodorovich.karamazov@gmail.com>
2017-01-19 23:22:55 +0900

    udpated.

---
+++ sip-server/sip-server.py
@@ -0,0 +1,25 @@
+class SIPServer():
+    pass
+
+class LocationServer(SIPServer):
+    pass
+
+class RegistrarServer(SIPServer):
+    pass
+
+class RedirectServer(SIPServer):
+    pass
+
+class ProxyServer(SIPServer):
+    pass
+
+class StatefulProxyServer(ProxyServer):
+    pass
+
+class StatelessProxyServer(ProxyServer):
+    pass
+
+class UAServer(SIPServer):
+    pass
+
+
