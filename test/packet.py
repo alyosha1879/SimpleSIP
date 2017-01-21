9def2ac543e5b7e45054770f66399c2ed69d06fe
Okano Yuu <alexei.fyodorovich.karamazov@gmail.com>
2017-01-22 00:01:27 +0900

    updated.

---
+++ test/packet.py
@@ -0,0 +1,17 @@
+def send_message(host, port, msg):
+    pass
+
+def send_response(host, port, msg):
+    pass
+
+def send_request(host, port, msg):
+    pass
+
+def handle_message(msg):
+    pass
+
+def handle_response(msg):
+    pass
+
+def handle_request(msg):
+    pass
