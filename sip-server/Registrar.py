c0b8758b62ca535af940a8e1fa18d24e5d6733ae
Okano Yuu <alexei.fyodorovich.karamazov@gmail.com>
2017-01-22 12:52:46 +0900

    updated.

---
+++ sip-server/Registrar.py
@@ -0,0 +1,16 @@
+class Registtar(UAS):
+
+    def __init__():
+        pass
+
+    def handle_request():
+        # only handle_register_request
+        assert(self.isMsgRequest("REGISTER"))
+
+    　　# RFC3261 "10.3 Processing REGISTER Requests"
+
+    def handle_response():
+        # only handle_register_response:
+        assert(self.isMsgRequest("REGISTER"))
+
+
