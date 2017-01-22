ccc892b0b823363266b209ec55cd92d7cedba1f1
Okano Yuu <alexei.fyodorovich.karamazov@gmail.com>
2017-01-22 19:32:08 +0900

    updated.

--- sip-server/Registrar.py
+++ sip-server/Registrar.py
@@ -1,16 +1,35 @@
 class Registtar(UAS):
 
     def __init__():
-        pass
 
-    def handle_request():
+        super(Registrar, self).__init__()
+
+
+    def handle_request(msg):
+
+        self.msg = msg
+
         # only handle_register_request
-        assert(self.isMsgRequest("REGISTER"))
+        assert(self.isMsgRequest(msg, "REGISTER"))
 
     　　# RFC3261 "10.3 Processing REGISTER Requests"
 
+        if self.fail_check_URI(msg):
+            # Not forward SIP Msg.
+            return
+
+        self.handl_require_header
+        self.auth_UAC
+
+        self.send_response(200)
+
     def handle_response():
+
         # only handle_register_response:
         assert(self.isMsgRequest("REGISTER"))
 
+    def fail_check_URI():
+        
+        host = msg.requestURI
 
+        host == 
